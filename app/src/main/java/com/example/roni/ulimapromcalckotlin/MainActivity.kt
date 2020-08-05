8a61e6a959fbf671fa7865152de7c743ac66a377
TheRoniOne <ronidani99@hotmail.com>
2020-08-05 17:16:51 -0500

    Refactorizado codigo

--- app/src/main/java/com/example/roni/ulimapromcalckotlin/MainActivity.kt
+++ app/src/main/java/com/example/roni/ulimapromcalckotlin/MainActivity.kt
@@ -2,43 +2,48 @@ package com.example.roni.ulimapromcalckotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
-import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Exception
-import java.lang.NullPointerException
 
 class MainActivity : AppCompatActivity() {
+    var eteInputTA : EditText? = null
+    var eteInputEP : EditText? = null
+    var butCalcular : Button? = null
+    var tviResultado : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val etInputTA : EditText = findViewById(R.id.etInputTA)
-        val etInputEP : EditText = findViewById(R.id.etInputEP)
-        val botonCalc : Button = findViewById(R.id.buttonCalcular)
-        val tvResultado : TextView = findViewById(R.id.tvResultado)
+        eteInputTA = findViewById(R.id.eteInputTA)
+        eteInputEP = findViewById(R.id.eteInputEP)
+        butCalcular = findViewById(R.id.butCalcular)
+        tviResultado = findViewById(R.id.tvResultado)
 
-        botonCalc.setOnClickListener {
-            calcResult()
-        }
     }
 
-    private fun calcResult() {
+    fun calcResult(v : View) {
         try {
-            var notaTA = etInputTA.text.toString().toInt()
-            var notaEP = etInputEP.text.toString().toInt()
+            val notaTA = eteInputTA!!.text.toString().toInt()
+            val notaEP = eteInputEP!!.text.toString().toInt()
+
             if ((notaTA in 0..20) and (notaEP in 0..20)){
                 var resultado = (10.5f - ((0.3f * notaTA) + (0.3f * notaEP))) / 0.4f
+
                 if (resultado > 0)
                 {
                     resultado += 1
                 }
-                var result : Int = resultado.toInt()
-                tvResultado.text = result.toString()
+
+                val result : Int = resultado.toInt()
+                tviResultado!!.text = result.toString()
+
             } else {
                 val msjError = AlertDialog.Builder(this)
+
                 msjError.setTitle("Ha ocurrido un problema")
                 msjError.setMessage("Porfavor ingresa correctamente tu nota de TA y EP")
                 msjError.setPositiveButton("Ok", null)
@@ -46,6 +51,7 @@ class MainActivity : AppCompatActivity() {
             }
         } catch (e : Exception){
             val msjError = AlertDialog.Builder(this)
+
             msjError.setTitle("Ha ocurrido un problema")
             msjError.setMessage("Porfavor ingresa correctamente tu nota de TA y EP")
             msjError.setPositiveButton("Ok", null)
